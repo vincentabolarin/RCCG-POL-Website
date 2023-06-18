8a577accd70e0182342fe1509ba5d96450dc0865
Vincent Abolarin <vincentabolarin@gmail.com>
2023-06-18 14:58:04 +0100

    setting up Firebase writing

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getDatabase, ref, set } from "firebase/database";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,4 +21,3 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
-const analytics = getAnalytics(app);
--- pages/house-fellowship-report-form.js
+++ pages/house-fellowship-report-form.js
@@ -1,3 +1,6 @@
+import { useRef } from 'react';
+import { getDatabase, ref, set } from "firebase/database";
+import firebaseConfig from '../firebaseConfig.js';
 import styles from '../styles/house-fellowship-report-form.module.scss';
 
 import Navbar from '../components/Navbar';
@@ -6,22 +9,49 @@ import houseFellowships from '../constants/house-fellowship-centres.js';
 import Image from 'next/image';
 
 const HouseFellowshipReportForm = () => {
+
+    const dateRef = useRef();
+    const centreRef = useRef();
+    const menRef = useRef();
+    const womenRef = useRef();
+    const childrenRef = useRef();
+    const offeringRef = useRef();
+
+    const submitData = (date, centre, men, women, children, offering) => {
+
+        // const date = dateRef.current.value;
+        // const centre = centreRef.current.value;
+        // const men = menRef.current.value;
+        // const women = womenRef.current.value;
+        // const children = childrenRef.current.value;
+        // const offering = offeringRef.current.value;
+
+        // const db = getDatabase();
+        // set(ref(db, 'centres/' + centre), {
+        //   date: date,
+        //   men: men,
+        //   women: women,
+        //   children: children,
+        //   offering: offering
+        // });
+    }
+
     return (
         <Navbar>
             <div>
                 <div className={styles.container}>
                     <h2>Please, fill out the report of your House Fellowship Centre.</h2>
 
-                    <form className="form">
+                    <form className="form" onSubmit={submitData()}>
                         <div>
                             <div className={styles.formItem}>
                                 <label htmlFor="date">Date</label>
-                                <input type="date" name="date" id="date" required />
+                                <input type="date" name="date" id="date" required ref={dateRef} />
                             </div>
                             
                             <div className={styles.formItem}>
                                 <label htmlFor="centre">Centre</label>
-                                <select name="centre" id="centre" required>
+                                <select name="centre" id="centre" required ref={centreRef}>
                                     <option value="select one" disabled>Select One</option>
                                     {
                                         houseFellowships.map((centre) => {
@@ -33,22 +63,22 @@ const HouseFellowshipReportForm = () => {
 
                             <div className={styles.formItem}>
                                 <label htmlFor="men">No. of Men</label>
-                                <input type="number" name="men" id="men" placeholder='Enter the number of men' required />
+                                <input type="number" name="men" id="men" placeholder='Enter the number of men' required ref={menRef} />
                             </div>
 
                             <div className={styles.formItem}>
                                 <label htmlFor="women">No. of Women</label>
-                                <input type="number" name="women" id="women" placeholder='Enter the number of women' required />
+                                <input type="number" name="women" id="women" placeholder='Enter the number of women' required ref={womenRef} />
                             </div>
 
                             <div className={styles.formItem}>
                                 <label htmlFor="children">No. of Children</label>
-                                <input type="number" name="children" id="children" placeholder='Enter the number of children' required />
+                                <input type="number" name="children" id="children" placeholder='Enter the number of children' required ref={childrenRef} />
                             </div>
 
                             <div className={styles.formItem}>
                                 <label htmlFor="offering">Offering (₦)</label>
-                                <input type="number" name="offering" id="offering" placeholder='Enter the total Offering amount' required />
+                                <input type="number" name="offering" id="offering" placeholder='Enter the total Offering amount' required ref={offeringRef} />
                             </div>
 
                             <div className={styles.formItem}>
